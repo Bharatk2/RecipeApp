2b218905c76646e27384fa736c2d61c7fc78dd5e
Bharat Kumar <57078122+Bharatk2@users.noreply.github.com>
2024-07-02 16:36:24 -0700

    Added project 2

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Admin on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
