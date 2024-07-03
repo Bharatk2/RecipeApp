3e72f8ebd2747cebddeebbe92512f906c73c2daa
Bharat Kumar <57078122+Bharatk2@users.noreply.github.com>
2024-07-02 17:03:41 -0700

    implemented fetch, add, delete, save data

--- RecipeApp/ViewModel/DessertCoreDataViewModel.swift
+++ RecipeApp/ViewModel/DessertCoreDataViewModel.swift
@@ -11,7 +11,7 @@ import SwiftUI
 class DessertCoreDataViewModel: ObservableObject {
     let container: NSPersistentContainer
     @Published var savedEntity: [DessertEntity] = []
-
+    
     init() {
         container = NSPersistentContainer(name: "Dessert")
         container.loadPersistentStores { description, error in
@@ -21,7 +21,42 @@ class DessertCoreDataViewModel: ObservableObject {
                 print("success")
             }
         }
-
+        fetchDessert()
     }
 
+    //fetch the request
+    func fetchDessert() {
+        let request =  NSFetchRequest<DessertEntity>(entityName:"DessertEntity")
+        
+        do {
+            savedEntity = try container.viewContext.fetch(request)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
+    //add the dessert
+    func addDessert(name: String) {
+        let newDessert = DessertEntity(context: container.viewContext)
+        newDessert.name = name
+        saveDessert()
+    }
+
+    //delete the dessert
+    func delete(index: IndexSet) {
+        guard let index = index.first else {return}
+        let entity = savedEntity[index]
+        container.viewContext.delete(entity)
+        saveDessert()
+    }
+    
+    //save the dessert
+    func saveDessert() {
+        do {
+            try  container.viewContext.save()
+            fetchDessert()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
 }
