2b32a3ca6bf93f62f61fc60b3b7b6ae375358d60
Bharat Kumar <57078122+Bharatk2@users.noreply.github.com>
2024-07-02 17:43:46 -0700

    Implemented main view & tab bar

---
+++ RecipeApp/Views/TabBar/TabBarView.swift
@@ -0,0 +1,34 @@
+//
+//  TabBarView.swift
+//  RecipeApp
+//
+//  Created by Admin on 7/2/24.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    @StateObject var dessertViewModel = DessertViewModel()
+    @StateObject var  dessertcore = DessertCoreDataViewModel()
+    @AppStorage("onboarding") var isOnBoardingViewActive: Bool = true
+    
+    var body: some View {
+        TabView {
+            HomeView()
+                .environmentObject(dessertViewModel)
+                .environmentObject(dessertcore)
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+        }
+        .onAppear(perform: {
+            isOnBoardingViewActive = true
+        })
+        .accentColor(.purple)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
