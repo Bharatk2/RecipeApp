4034c1bd4b67e8054deed95c10eceba7389ac40a
Bharat Kumar <57078122+Bharatk2@users.noreply.github.com>
2024-07-02 18:39:31 -0700

    Implemented set up and tear down

--- RecipeAppTests/RecipeAppTests.swift
+++ RecipeAppTests/RecipeAppTests.swift
@@ -8,28 +8,17 @@
 import XCTest
 
 final class RecipeAppTests: XCTestCase {
-
+    let timeout: TimeInterval = 5
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        MockURLProtocol.requestHandler = nil
+        // using ephemeral configuration to implement testing in order to avoid storing data to disk.
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [MockURLProtocol.self]
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+        MockURLProtocol.requestHandler = nil
     }
 
 }
