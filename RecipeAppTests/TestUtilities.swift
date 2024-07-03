7a85b426c37c38f2831d54ac54b7002bc0356aa7
Bharat Kumar <57078122+Bharatk2@users.noreply.github.com>
2024-07-02 18:37:07 -0700

    Added MockURLProtocol to test

---
+++ RecipeAppTests/TestUtilities.swift
@@ -0,0 +1,39 @@
+//
+//  TestUtilities.swift
+//  RecipeAppTests
+//
+//  Created by Admin on 7/2/24.
+//
+
+import Foundation
+import XCTest
+
+class MockURLProtocol: URLProtocol {
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else {
+            XCTFail("Handler is unavailable.")
+            return
+        }
+        
+        do {
+            let (response, data) = try handler(request)
+            self.client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            self.client?.urlProtocol(self, didLoad: data)
+            self.client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            self.client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+    
+    override func stopLoading() {}
+}
