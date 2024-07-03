6e1b2151b4c9d673f42cd5f3c58ad9811555594e
Bharat Kumar <57078122+Bharatk2@users.noreply.github.com>
2024-07-02 18:45:28 -0700

    Implemented all desert category test

--- RecipeAppTests/RecipeAppTests.swift
+++ RecipeAppTests/RecipeAppTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import RecipeApp
 
 final class RecipeAppTests: XCTestCase {
     let timeout: TimeInterval = 5
@@ -21,4 +22,29 @@ final class RecipeAppTests: XCTestCase {
         MockURLProtocol.requestHandler = nil
     }
 
+    /* 
+     This test verifies that the getAllDessertCategory method of the APICaller class
+      correctly fetches and decodes a list of dessert categories from a remote API.
+     */
+    func testGetAllDessertCategory() async throws {
+        let mockMeals = [
+            MealData(mealID: "53049", meal: "Apam balik", mealThumb: "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg")
+        ]
+        
+        let mockMealModel = Meal(meals: mockMeals)
+        let mockData = try JSONEncoder().encode(mockMealModel)
+        
+        MockURLProtocol.requestHandler = { request in
+            let response = HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: nil)!
+            return (response, mockData)
+        }
+        
+        let mockSession = URLSession(configuration: URLSessionConfiguration.ephemeral)
+        let apiCaller = APICaller(session: mockSession)
+        let meals = try await apiCaller.getAllDessertCategory()
+        
+        XCTAssertEqual(meals.first?.mealID, "53049")
+        XCTAssertEqual(meals.first?.meal, "Apam balik")
+        XCTAssertEqual(meals.first?.mealThumb, "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg")
+    }
 }
