7ddb276028199c80b771d2a13f234e47b411d3fb
Bharat Kumar <57078122+Bharatk2@users.noreply.github.com>
2024-07-02 17:27:27 -0700

    implemented homeview with scroll view

--- RecipeApp/Views/Home/HomeView.swift
+++ RecipeApp/Views/Home/HomeView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct HomeView: View {
+    @EnvironmentObject var dessertViewModel: DessertViewModel
+    @State private var searchText: String = ""
+    var columns = [GridItem(.adaptive(minimum: 150),spacing: 40)]
+    
+    var filteredMeals: [MealData] {
+        if searchText.isEmpty {
+            return dessertViewModel.meals
+        } else {
+            return dessertViewModel.meals.filter { $0.meal.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                ScrollView {
+                    LazyVGrid(columns: columns) {
+                        ForEach(filteredMeals, id: \.self) { meal in
+                            MealScrollView(mealModel: meal)
+                        }
+                    }
+                }
+                .navigationBarTitle("Home🍰")
+            }
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
-#Preview {
-    HomeView()
+struct HomeView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        let dessertPreviewCheck = DessertViewModel()
+        HomeView()
+            .environmentObject(dessertPreviewCheck)
+    }
 }
+
