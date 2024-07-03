056479669e6c5bcf203d0eec8c1393d316f494eb
Bharat Kumar <57078122+Bharatk2@users.noreply.github.com>
2024-07-02 18:49:34 -0700

    Implemented UI Test

--- RecipeAppUITests/RecipeAppUITests.swift
+++ RecipeAppUITests/RecipeAppUITests.swift
@@ -6,30 +6,32 @@
 //
 
 import XCTest
+import SwiftUI
+@testable import RecipeApp
 
 final class RecipeAppUITests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
+        
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
@@ -37,5 +39,20 @@ final class RecipeAppUITests: XCTestCase {
                 XCUIApplication().launch()
             }
         }
+    }
+    
+    func testWelcomeScreenElements() throws {
+        let app = XCUIApplication()
+        app.launch()
+        print(app.debugDescription)
+        let hungryLabel = app.staticTexts["hungryText"]
+        XCTAssertEqual(hungryLabel.label, "Are You Hungry?")
+        let image = app.images["HungryImage"]
+        XCTAssertEqual(image.label, "Hungry")
+        let button = app.buttons["LetsEatButton"]
+        XCTAssertEqual(button.label, "Let's Eat")
+        XCTAssert(button.exists)
+        
+        button.tap()
     }
 }
